4d5463b6f7063dd5e8d72a6a2b48b897cd773eb3
Tim <halltim@pdx.edu>
2023-10-05 23:53:16 -0700

    formating fix. added NaN condition

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -4,10 +4,13 @@ const output = document.querySelector('#status')
 elem.addEventListener('input', handleInput);
 function handleInput(event) {
     const input = event.target.value;
+    
     if(input.length === 0){
         output.textContent = " ";
+    } else if(isNaN(input)){ //doesn't really do anything since you can't enter letters into number input
+        setOutputText('Numeric characters only.', false);
     } else if (input < 0) {
-        setOutputText('Must be a positive number')
+        setOutputText('Must be a positive number', false);
     } else {
         for(let i = 0; i < input.length / 2; i++){
             if(input[i] !== input[input.length - (1 + i)]){

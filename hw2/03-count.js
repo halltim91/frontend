b6d3adfcf56961a1cb79aa32c87aa9f1d3dd1f64
Tim <halltim@pdx.edu>
2023-10-16 23:02:51 -0700

    Added exercise 3

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,17 @@
-// Add your code here
+const textInput = document.getElementById('input');
+const text = document.getElementById('text');
+const origText = text.innerHTML;
 
-input.addEventListener('keydown', handleKeyDown);
+textInput.addEventListener('keydown', handleKeyDown);
+
+function handleKeyDown(event) {
+    if(event.key === 'Enter'){
+        const word = event.target.value.trim().split(' ')[0];
+        if(word.length > 0){
+            const temp = origText.replaceAll(word, "<mark>" + word + "</mark>");
+            text.innerHTML = temp;
+        } else {
+            text.innerHTML = origText;
+        }
+    }
+}

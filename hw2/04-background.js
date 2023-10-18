26eeba9257a3a6dae0098199d59c1cdd4825970c
Tim <halltim@pdx.edu>
2023-10-17 22:34:59 -0700

    Added exercise 4

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,45 @@
-// Add your code here
+const button = document.getElementById('button');
+const input = document.getElementById('input');
+const body = document.querySelector('body');
+
+let interval = setInterval(setBackgroundColor, 3000);
+
+window.addEventListener('load', onload);
+button.addEventListener('click', onclick);
+
+function onload(event) {
+    setBackgroundColor();
+}
+
+function onclick(event) {
+    if(event.target.value === 'Start'){
+        updateButton(event.target, true);
+        const time = 1000 * Number(input.value);
+        interval = setInterval(setBackgroundColor, time);
+    }
+    else {
+        updateButton(event.target, false);
+        clearInterval(interval);
+    }
+}
+
+function setBackgroundColor(){
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    const a =  .5 + (Math.random() * .4);
+    const color = 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
+    body.style.backgroundColor = color;
+}
+
+function updateButton(btn, isRunning){
+    if(isRunning){
+        btn.value = 'Stop';
+        btn.classList.add('bg-danger');
+        btn.classList.remove('bg-primary');
+    } else {
+        btn.value = 'Start';
+        btn.classList.add('bg-primary');
+        btn.classList.remove('bg-danger');
+    }
+}

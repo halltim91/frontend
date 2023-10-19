dfbe4d0324d26dbe1fa9a0cc2870e611b81787ea
Tim <halltim@pdx.edu>
2023-10-18 22:20:04 -0700

    Added exercise 5

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,18 @@
-// Add your code here
+const form = document.querySelector("form");
+const elements = form.elements;
+
+form.addEventListener("submit", onsubmit);
+function onsubmit(event) {
+    let output = '';
+    let e;
+    for(let i = 0; i < elements.length; i++){
+        e = elements[i];
+        if(e.id !== ''){
+            output += e.id + ': ';
+            output += e.type === 'checkbox' ? e.checked : e.value;
+            output += '\n';
+        }
+    }
+    console.log(output);
+    event.preventDefault();
+}

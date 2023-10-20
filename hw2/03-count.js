60c32b26e3c50a577161795f822a0adf7566d25b
Tim <halltim@pdx.edu>
2023-10-19 21:21:43 -0700

    fix bug where search highlights parts of words

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -8,7 +8,7 @@ function handleKeyDown(event) {
     if(event.key === 'Enter'){
         const word = event.target.value.trim().split(' ')[0]; //ignores anything after first word
         if(word.length > 0){
-            text.innerHTML = origText.replaceAll(word, '<mark>' + word + '</mark>');
+            text.innerHTML = origText.replaceAll(' ' + word + ' ', ' <mark>' + word + '</mark> ');
         } else {
             text.innerHTML = origText;
         }

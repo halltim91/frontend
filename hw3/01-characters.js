e35c6f6df7d4a4d03941d35c080b72e28a034e77
Tim <halltim@pdx.edu>
2023-10-26 14:32:28 -0700

    Implemented exercise 1

--- hw3/01-characters.js
+++ hw3/01-characters.js
@@ -1,4 +1,36 @@
 // url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
+const content = document.getElementById('content');
 
-fetch(url);
+fetch(url)
+    .then((resp) =>resp.json())
+    .then(displaystuff)
+    .catch((err) => console.log(`error getting data: ${err}`));
+
+function displaystuff(data){
+    console.log(data);
+    let character;
+    for( let i = 0; i < data.length; i++){
+        content.appendChild(createElement(data[i]));
+    }
+    return data;
+}
+
+function createElement(character){
+    const div = document.createElement("div");
+    const img = document.createElement('img');
+    const name = document.createElement('h4');
+    const title = document.createElement('p');
+    
+    div.classList.add('mycard');
+    img.src = character.imageUrl;
+    img.alt = character.image;
+    name.innerText = character.fullName;
+    title.innerText = character.title;
+    
+    div.appendChild(img);
+    div.appendChild(name);
+    div.appendChild(title);
+    
+    return div;
+}

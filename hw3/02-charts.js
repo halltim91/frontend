a19ca505a1da7dbb345580efc69cb59d7efd4dec
Tim <halltim@pdx.edu>
2023-10-27 10:41:30 -0700

    Implemented exercise 2

--- hw3/02-charts.js
+++ hw3/02-charts.js
@@ -35,18 +35,97 @@ const renderChart = () => {
   new Chart(donutChart, {
     type: 'doughnut',
     data: {
-      labels: ['label', 'label', 'label', 'label'],
+      labels: families,
       datasets: [
         {
-          label: 'My First Dataset',
-          data: [1, 12, 33, 5],
+          label: 'Game of Thrones Houses',
+          data: numMembers,
           backgroundColor: backgroundColors,
           borderColor: borderColors,
           borderWidth: 1,
         },
       ],
     },
+    options: {
+      legend: {
+        display: false
+      }
+    }
   });
 };
 
-renderChart();
+fetch(url)
+  .then((resp) =>resp.json())
+  .then(findDistro)
+  .then(renderChart)
+  .catch((err) => console.log(`error getting data: ${err}`));
+
+
+let families = [];
+let numMembers = [];
+
+/* Sorts all the characters either into houses or non-house family group.
+*/
+function findDistro(data){
+  // console.log(data);
+  for(let i = 0; i < data.length; i++){
+    let family = data[i].family;
+    let index = matchHouse(family);
+
+    //consolidate families that don't have 'House ' in the name
+    if(index === -1 && !family.includes('House ')){
+      family = 'Non-House Family';
+      index = families.findIndex((e) => e.includes(family));
+    }
+
+    if(index < 0){
+      families.push(family);
+      numMembers.push(1);
+    } else {
+        numMembers[index] += 1;
+    }
+  }
+  for(let u = 0; u < families.length; u++)
+    console.log(families[u], numMembers[u]);
+  console.log(families);
+  console.log(numMembers);
+  return data;
+}
+
+/*
+Looks for any houses that are already in the list, also check for any 
+house names that are similarly spelt
+*/
+function matchHouse(fam) {
+  //if the name is already in the list or its a part of another name
+  const i = families.findIndex((e) => e.includes(fam)); 
+  if(i > -1) return i;
+  //check if there are names spelt similarly (Typos)
+  const j = families.findIndex((e) => levenshteinDistance(fam.replace('House ',''), e.replace('House ', '')) === 1);
+  return j;
+}
+/*
+ Levenshtein Distance: Finds the number of differences between 2 strings
+ From https://www.tutorialspoint.com/levenshtein-distance-in-javascript
+*/
+ function levenshteinDistance(str1 = '', str2 = '') {
+  const track = Array(str2.length + 1).fill(null).map(() =>
+  Array(str1.length + 1).fill(null));
+  for (let i = 0; i <= str1.length; i += 1) {
+     track[0][i] = i;
+  }
+  for (let j = 0; j <= str2.length; j += 1) {
+     track[j][0] = j;
+  }
+  for (let j = 1; j <= str2.length; j += 1) {
+     for (let i = 1; i <= str1.length; i += 1) {
+        const indicator = str1[i - 1] === str2[j - 1] ? 0 : 1;
+        track[j][i] = Math.min(
+           track[j][i - 1] + 1, // deletion
+           track[j - 1][i] + 1, // insertion
+           track[j - 1][i - 1] + indicator, // substitution
+        );
+     }
+  }
+  return track[str2.length][str1.length];
+};
